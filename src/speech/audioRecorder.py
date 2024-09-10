b07721ff731f27d43c84434a08204f2c21e0db53
Konrad <konradseime@gmail.com>
2024-09-10 02:49:34 +0200

    Adding audio recorder

---
+++ src/speech/audioRecorder.py
@@ -0,0 +1,73 @@
+
+import pyaudio
+import time
+import numpy as np
+import sys
+
+
+class AudioRecorder:
+    def __init__(self, chunk_size=1024, rate=16000, channels=1, silence_threshold=500, max_silence_duration=5):
+        self.chunk_size = chunk_size
+        self.rate = rate
+        self.channels = channels
+        self.silence_threshold = silence_threshold
+        self.max_silence_duration = max_silence_duration
+
+        self.audio_chunks = []
+
+    def start_recording(self):
+        self.audio = pyaudio.PyAudio()
+        self.stream = self.audio.open(format=pyaudio.paInt16, channels=self.channels, rate=self.rate, input=True, frames_per_buffer=self.chunk_size)
+        self.audio_chunks = []
+        print("Recording started")
+
+
+    def stop_recording(self):
+        self.stream.stop_stream()
+        self.stream.close()
+        print("Recording stopped")
+
+    def silent(self, data):
+        audio_data = np.frombuffer(data, dtype=np.int16)
+        if np.isnan(audio_data).any():
+            return True
+        rms = np.sqrt(np.mean(audio_data ** 2))
+        # sys.stdout.write(f"\rRMS: {rms}")
+        # sys.stdout.flush()
+        return rms < self.silence_threshold
+    
+    def record(self):
+        self.start_recording()
+
+        self.audio_chunks = []
+        silence_start = None
+
+        MERGE_SIZE = 20
+
+        try:
+            while True:
+                data = self.stream.read(self.chunk_size)
+                self.audio_chunks.append(data)
+            
+                if self.silent(data):
+
+                    if silence_start is None:
+                        silence_start = time.time()
+                    
+                    if len(self.audio_chunks) >= MERGE_SIZE and time.time() - silence_start > 0.5:
+                        yield b''.join(self.audio_chunks)  
+                        self.audio_chunks = []
+                        
+
+                    elif time.time() - silence_start > self.max_silence_duration:   
+                        print(f"Silence detected for more than {self.max_silence_duration} seconds, stopping recording.")
+                        break
+                else:
+                    silence_start = None
+
+        except KeyboardInterrupt:
+            print("Recording stopped by user.")
+        finally:
+            self.stop_recording()
+
+        return self.audio_chunks
