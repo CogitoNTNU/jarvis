f132781dd58765bf745968db34d3932f8981af07
Jon Bergland <jobergl@stud.ntnu.no>
2024-09-19 18:25:13 +0200

    feat: add route for sending a message to llm

--- src/langchain/main.py
+++ src/langchain/main.py
@@ -1,10 +1,10 @@
-from flask import Flask
-import agent
-import models
+from flask import Flask, request
+from src.langchain.agent import Agent
+from src.langchain.models import Model
 
-jarvis = agent.Agent(models.Model.gpt_35)
+jarvis = Agent(Model.gpt_35)
 
-def prompt_jarvis(prompt: str) -> str:
+def prompt_jarvis(prompt) -> str:
     return jarvis.run(prompt)
 
 
@@ -14,4 +14,15 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello from Jarvis'
 
+@app.route('/send_message', methods=['POST', 'GET'])
+def llm_request():
+    if(request.method == 'POST'):
+        data = request.json
+        print(data)
+        print(data['message'])
+        ai_message = prompt_jarvis(data['message'])
+        return {"message": ai_message}
+
+if __name__ == '__main__':
+    app.run(debug=True, port='placeholder', host='placeholder')
 
