397faa025f45938a5aac9cbadb8015d890b551b8
Konrad <konradseime@gmail.com>
2024-09-20 19:22:41 +0200

    Added threading and changed language of model

--- src/speech/speech_to_text.py
+++ src/speech/speech_to_text.py
@@ -5,29 +5,39 @@ import numpy as np
 import wave
 from audioRecorder import AudioRecorder
 import time
+from threading import Thread
 
 load_dotenv()
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 
-def create_tmp_wav_file(chunk, rate=16000, channels=1):
-    with wave.open("tmp.wav", 'wb') as wav_file:
+def create_tmp_wav_file(chunk, rate=16000, channels=1, path="tmp.wav"):
+    with wave.open(path, 'wb') as wav_file:
         wav_file.setnchannels(channels)  # Mono audio
         wav_file.setsampwidth(2)  # 2 bytes per sample, assuming 16-bit audio
         wav_file.setframerate(rate)  # Assuming 16kHz sample rate
         wav_file.writeframes(chunk)
 
-def remove_tmp_wav_file():
-    if os.path.exists("tmp.wav"):
-        os.remove("tmp.wav")
+def remove_tmp_wav_file(index=None):
+    if index is not None:
+        if os.path.exists(f"tmp{index}.wav"):
+            os.remove(f"tmp{index}.wav")
+    else:
+        if os.path.exists("tmp.wav"):
+            os.remove("tmp.wav")
 
 def speech_to_text(audio_file):
+    prompt="Transcribe the following Norwegian speech to text, the sentances may be cut off, do not make up words or fill in the sentances"
+
     transcription = client.audio.transcriptions.create(
     model="whisper-1", 
-    file=audio_file
+    file=audio_file, 
+    language="no", 
+    prompt=prompt,
     )
-
+    transcription.text.replace(prompt, "")
     return transcription
+
 
 def path_to_audio_file(path):
     audio_file = open(path, "rb")
@@ -44,34 +54,47 @@ def chunks_to_full_audio(chunks):
     return b"".join(chunks)
 
 
+def handle_chunk(chunk, index):
+    create_tmp_wav_file(chunk, path=f"tmp{index}.wav")
+    audio_file = path_to_audio_file(f"tmp{index}.wav")
+
+    text.append(speech_to_text(audio_file=audio_file))
+    audio_file.close()
+    print(text[-1].text)
+    remove_tmp_wav_file(index)
+
 
 if __name__ == "__main__":
-    CHUNK_SIZE = 1024  # Number of frames in a buffer
-    RATE = 16000       # 16 000 Hz is a common rate for speech processing
-    CHANNELS = 1       # Mono audio
-    SILENCE_THRESHOLD = 8  # Used to detect silence for stopping recording
-    MAX_SILENCE_DURATION = 5  # Seconds of silence to stop recording
+    import sys
+    if len(sys.argv) ==1:
 
-    recorder = AudioRecorder(chunk_size=CHUNK_SIZE, rate=RATE, channels=CHANNELS, silence_threshold=SILENCE_THRESHOLD, max_silence_duration=MAX_SILENCE_DURATION)
+        CHUNK_SIZE = 1024  # Number of frames in a buffer
+        RATE = 16000       # 16 000 Hz is a common rate for speech processing
+        CHANNELS = 1       # Mono audio
+        SILENCE_THRESHOLD = 25  # Used to detect silence for stopping recording
+        MAX_SILENCE_DURATION = 5  # Seconds of silence to stop recording
 
-    text = []
-    for chunk in recorder.record():
-        create_tmp_wav_file(chunk)
-        audio_file = path_to_audio_file("tmp.wav")
-        text.append(speech_to_text(audio_file=audio_file))
+        recorder = AudioRecorder(chunk_size=CHUNK_SIZE, rate=RATE, channels=CHANNELS, silence_threshold=SILENCE_THRESHOLD, max_silence_duration=MAX_SILENCE_DURATION)
+        
+        text = []
+        index = 0
+        for chunk in recorder.record(240):
+            t = Thread(target=handle_chunk, args=(chunk,index))
+            index += 1
+            t.start()
+        
+        time.sleep(2)
+
+        remove_tmp_wav_file()
+    
+    else:
+        audio_file = path_to_audio_file(sys.argv[1])
+        transcription = speech_to_text(audio_file)
+        print(transcription.text)
         audio_file.close()
-        print(text[-1].text)
-
-    time.sleep(2)
-
-    remove_tmp_wav_file()
 
         
     # audio_file = path_to_audio_file("nb-whisper-main/nb-whisper-main/audio/erna.mp3")
     # transcription = speech_to_text(audio_file)
     # print(transcription.text)
     # audio_file.close()
-
-
-
-
