9597c523d76590cc0dd4131cd9517e5af1d1f857
Jon Bergland <jobergl@stud.ntnu.no>
2024-09-26 19:40:17 +0200

    feat: add method to generate random UUID

--- core/static/socketEvents.js
+++ core/static/socketEvents.js
@@ -40,6 +40,13 @@ let openStreamedAIMessage = () => {
     // append to its innerText as the streams come through.
 }
 
+let generateUUID = () => {
+    return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
+        (+c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> +c / 4).toString(16)
+    );
+}
+
+
 // Remember to parse the streamed response
 
 /*
