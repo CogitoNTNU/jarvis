2c1616b0ad7b63759bdc003b616bcf1357a695c7
EldarAlvik <eldar.alvik@outlook.com>
2024-10-01 18:21:42 +0200

    Merge branch 'main' of https://github.com/CogitoNTNU/jarvis

--- core/config.py
+++ core/config.py
@@ -4,3 +4,12 @@ load_dotenv()
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 PORT = os.getenv("PORT")
+LANGSMITH_API_KEY: str = os.getenv(key="LANGSMITH_API_KEY")
+
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_ENDPOINT"] = "https://eu.api.smith.langchain.com"
+os.environ["LANGCHAIN_API_KEY"] = LANGSMITH_API_KEY
+
+if __name__ == "__main__":
+    print(f"[INFO] OPENAI_API_KEY: {OPENAI_API_KEY}")
+    print(f"[INFO] LANGSMITH_API_KEY: {LANGSMITH_API_KEY}")
