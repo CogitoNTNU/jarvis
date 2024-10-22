262d34c87408d64b726981849f2d499ee32cc126
EldarAlvik <eldar.alvik@outlook.com>
2024-10-22 20:02:05 +0200

    Feat: added create_agent function

--- core/graphtools.py
+++ core/graphtools.py
@@ -11,6 +11,7 @@ from langchain_core.messages import (
     ToolMessage,
 )
 import operator
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 
 class AgentState(TypedDict):
     messages: Annotated[Sequence[BaseMessage], operator.add]
@@ -50,3 +51,26 @@ class graphtool:
             # track the sender so we know who to pass to next.
             "sender": name,
         }
+    
+
+    def create_agent(llm, tools, system_message: str):
+        """Create an agent"""
+        prompt = ChatPromptTemplate.from_messages(
+            [
+                (
+                    "system",
+                    "You are a helpful AI assistant, collaborating with other assistants."
+                    " Use the provided tools to progress towards answering the question."
+                    " If you are unable to fully answer, that's OK, another assistant with different tools "
+                    " will help where you left off. Execute what you can to make progress."
+                    " If you or any of the other assistants have the final answer or deliverable,"
+                    " prefix your response with FINAL ANSWER so the team knows to stop."
+                    " You have access to the following tools: {tool_names}.\n{system_message}",
+                ),
+                MessagesPlaceholder(variable_name="messages"),
+            ]
+        )
+        prompt = prompt.partial(system_message=system_message)
+        prompt = prompt.partial(tool_names=", ".join([tool.name for tool in tools]))
+        return prompt | llm.bind_tools(tools)
+    
