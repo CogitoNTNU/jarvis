3db2118c0ca2be1e2f0777e49f017d48c62d3bef
EldarAlvik <eldar.alvik@outlook.com>
2024-10-23 15:54:07 +0200

    Feat: added tme related tools

--- core/tools/tools.py
+++ core/tools/tools.py
@@ -6,6 +6,10 @@ import tools.read_file as read_file
 import tools.read_pdf as read_pdf
 import tools.weather as weather
 import tools.google_calender_create as create_calender_event
+import tools.create_time_to_iso as create_time_to_iso_format
+import tools.current_time_iso as current_time_iso_format
+import tools.add_time as add_time
+
 
 def get_tools() -> list[StructuredTool]:
     tools = []
@@ -16,5 +20,7 @@ def get_tools() -> list[StructuredTool]:
     tools.append(read_pdf.get_tool())
     tools.append(weather.get_tool())
     tools.append(create_calender_event.get_tool())
+    tools.append(create_time_to_iso_format.get_tool())
+    tools.append(current_time_iso_format.get_tool())
 
     return tools
