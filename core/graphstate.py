09869cf78f458a55aa79645d6de379ab6fb5e4b6
Jon Bergland <jobergl@stud.ntnu.no>
2024-10-29 18:37:08 +0100

    feat: add a variable for data in GraphState

--- core/graphstate.py
+++ core/graphstate.py
@@ -8,3 +8,4 @@ class GraphState(TypedDict):
     # in the annotation defines how this state key should be updated
     # (in this case, it appends messages to the list, rather than overwriting them)
     messages: Annotated[list, add_messages]
+    data: dict
