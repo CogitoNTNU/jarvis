24eaa8c17c609c685e485e5fff45c898012e6dee
Jon Bergland <jobergl@stud.ntnu.no>
2024-10-29 18:54:53 +0100

    feat: add nodes for deciding tool and generating response

--- core/noder.py
+++ core/noder.py
@@ -1,11 +1,12 @@
 from graphstate import GraphState
 from Agents.simpleagent import SimpleAgent
 from langchain.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 
 
 def jarvis_agent(state: GraphState):
     """Agent to determine how to answer user question"""
-    promt = PromptTemplate(
+    prompt = PromptTemplate(
         template= """
         Lorem ipsum....
         Here are previous messages:
@@ -19,5 +20,37 @@ def jarvis_agent(state: GraphState):
         TODO: Create options for the llm to answer
 
         Answer with the option name and nothing else
-        """
+        """,
     )
+    chain = prompt | SimpleAgent.llm | StrOutputParser()
+
+    return chain.invoke({"messages": state["messages"], "data": state.get("data", {})})
+
+def tool_decider(state: GraphState):
+    """Agent to determine what tool to use"""
+    prompt = PromptTemplate(
+        template= """
+        Lorem ipsum....
+        Here are previous messages:
+        
+        Message: {messages}
+
+        Data currently accumulated: 
+
+        Data: {data}
+
+        TODO: Create options for the llm to answer
+
+        Answer with the option name and nothing else
+        """,
+    )
+
+    chain = prompt | SimpleAgent.llm | StrOutputParser()
+
+    return chain.invoke({"messages": state["messages"], "data": state.get("data", {})})
+
+def response_generator(state: GraphState):
+    """Agent that generates a response to user based on user request 
+    and possible data from tool calls"""
+    #TODO Implement
+    return ""
