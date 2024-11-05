f8246416d92f7fe870d08b6e8572c15ad9a934e5
EldarAlvik <eldar.alvik@outlook.com>
2024-11-05 19:11:14 +0100

    Refactro: Moved calender_agent

--- core/noder.py
+++ core/noder.py
@@ -4,6 +4,7 @@ from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from typing import Literal
 from tools.tools import get_tools
+
 def jarvis_agent(state: GraphState):
     """Agent to determine how to answer user question"""
     prompt = PromptTemplate(
@@ -83,32 +84,3 @@ def response_generator(state: GraphState):
     return {"messages": [response]}
 
 
-
-def calender_agent(state: GraphState):
-    """Agent that handles all actions in the calender"""
-    prompt = PromptTemplate(
-        template= """
-        Your job is to handle any calender events that the user requests.
-        you have acces to the google calender api and can create, delete 
-
-        Here are previous messages:
-        
-        Message: {messages}
-
-        Data currently accumulated: 
-
-        Data: {data}
-
-        See calender events are in:
-        
-        Calendere evnts:{calender_events}
-
-        Your options are the following:
-        to generate a calender event
-        Answer with the option name and nothing else
-        """,
-    )
-    chain = prompt | ToolsAgent.agent | StrOutputParser()
-    response = chain.invoke({
-        "messages": state["messages"], "data": state.get("data", {}), "calender_events": get_tools()[]})
-    return {"tool_decision": response}
