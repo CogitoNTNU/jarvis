1093afa3686dd07fb95866e6e301f40aba34507a
erlendska <erlend.ulvund.skaarberg@gmail.com>
2025-02-20 18:20:06 +0100

    build: added linting and formatting with eslint and prettier

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,9 +1,7 @@
-import React from 'react';
+import React from "react";
 
 function App() {
-  return (
-    <h1 className="font-bold italic">Heheheh</h1>
-  )
+  return <h1 className="font-bold italic">Heheheh</h1>;
 }
 
 export default App;
--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -3,12 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-export default defineConfig(
-	{
-		plugins:
-			[
-				react(),
-				tailwindcss(),
-			],
-	}
-);
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+});
