8cf9c49e7269a40c501c9e6b35b0b455a0df4637
William M Schmidt <35806072+WilliamMRS@users.noreply.github.com>
2025-03-18 18:43:04 +0100

    Delete core/Agents directory

--- core/Agents/model.py
+++
@@ -1,9 +0,0 @@
-class LLMType:
-    chat_open_ai = "ChatOpenAI"
-
-class Model:
-    # OpenAI models
-    gpt_4o_mini: tuple[str, LLMType] = ("gpt-4o-mini", LLMType.chat_open_ai)
-    gpt_4o: tuple[str, LLMType] = ("gpt-4o", LLMType.chat_open_ai)
-    gpt_35: tuple[str, LLMType] = ("gpt-3.5-turbo", LLMType.chat_open_ai)
-    gpt_o3_mini: tuple[str, LLMType] = ("o3-mini", LLMType.chat_open_ai)
