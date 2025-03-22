7a80f8cd4fddd072e4c2fddb9bb0ffd25909d7c9
DuckLeQuack <muh.muwaffaq@gmail.com>
2025-03-22 13:45:34 +0100

    JARVIS now can see :D

--- core/agents/neo_think_agent.py
+++ core/agents/neo_think_agent.py
@@ -19,6 +19,7 @@ from langchain_community.tools.tavily_search import TavilySearchResults
 # Custom tool imports
 from tools.add_tool import add # Adds 2 numbers together
 from tools.youtube_transcript import youtube_transcript
+from tools.vision import vision
 
 from agents.WebSocketAgent import WebSocketAgent # Superclass
 
@@ -35,7 +36,7 @@ class NeoThinkAgent(WebSocketAgent):
         # Defining the checkpoint memory saver.
         memory = MemorySaver()
         # Tools list
-        tools = [add, youtube_transcript]
+        tools = [add, youtube_transcript, vision]
 
         if os.getenv("TAVILY_API_KEY"):
             # Defining the tavily web-search tool
---
+++ core/tools/vision.py
@@ -0,0 +1,39 @@
+from openai import OpenAI
+import os
+import base64
+
+import config
+from langchain_core.tools import tool
+from langchain_core.tools.structured import StructuredTool
+
+@tool
+def vision(user_text: str, file_path: str):
+    """
+    Use this tool to generate a response based on an image url and user text.
+    """
+
+    #Only for URL / online images
+    messages = [
+        {
+            "role": "user",
+            "content": [
+                {"type": "text", "text": user_text},
+                {"type": "image_url","image_url": {"url": file_path}}
+            ]
+        }
+    ]
+
+    client = OpenAI(api_key=config.OPENAI_API_KEY)
+
+    response = client.chat.completions.create(
+        model="gpt-4o",
+        messages=messages,
+        # temperature=0.0,
+    )
+
+    message = response.choices[0].message.content
+
+    return message
+
+def get_tool() -> StructuredTool:
+    return vision
