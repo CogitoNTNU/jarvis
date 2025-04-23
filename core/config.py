2c4624c89f582fdb4ca2c5bb762a7aebd2f85c8d
EldarAlvik <eldar.alvik@outlook.com>
2025-04-23 17:39:03 +0200

    refactor(env): remove LANGSMITH_API_KEY from example and config

--- core/config.py
+++ core/config.py
@@ -9,9 +9,6 @@ PORT = os.getenv("PORT")
 PERPLEXITY_API_KEY = os.getenv("PERPLEXITY_API_KEY")
 
 
-#add langsmith api to env as LANGSMITH_API_KEY = "your_api_key" on EU server
-LANGSMITH_API_KEY = os.getenv("LANGSMITH_API_KEY", "no_key")
-print(LANGSMITH_API_KEY)
 
 #configure pheonix otel
 os.environ['PHOENIX_PROJECT_NAME'] = "Jarvis"
@@ -22,6 +19,4 @@ tracer_provider = register(
 
 if __name__ == "__main__":
     print(f"[INFO] OPENAI_API_KEY: {OPENAI_API_KEY}")
-    if(LANGSMITH_API_KEY):
-        print(f"[INFO] LANGSMITH_API_KEY: {LANGSMITH_API_KEY}")
 
