7a115c9a1722a43c10725842b150592ec866e0c3
EldarAlvik <eldar.alvik@outlook.com>
2025-04-24 19:02:28 +0200

    refactor(tools): remove weather tool and its references

--- core/tools/tools.py
+++ core/tools/tools.py
@@ -4,7 +4,6 @@ import tools.web_search as web_search
 import tools.find_files as find_files
 import tools.read_file as read_file
 import tools.read_pdf as read_pdf
-import tools.weather as weather
 import tools.google_calendar_create as create_calendar_event
 import tools.google_calendar_read as read_calendar_event
 import tools.create_time_to_iso as create_time_to_iso_format
@@ -23,7 +22,6 @@ def get_tools() -> list[StructuredTool]:
     tools.append(find_files.get_tool())
     tools.append(read_file.get_tool())
     tools.append(read_pdf.get_tool())
-    tools.append(weather.get_tool())
     tools.append(create_calendar_event.get_tool())
     tools.append(read_calendar_event.get_tool())
     tools.append(create_time_to_iso_format.get_tool())
@@ -37,7 +35,6 @@ def get_tools() -> list[StructuredTool]:
 
 def get_perplexity_based_tools() -> list[StructuredTool]:
     tools = []
-    tools.append(weather.get_tool())
     tools.append(web_search.get_tool())
     tools.append(rag_search.get_tool())
 
